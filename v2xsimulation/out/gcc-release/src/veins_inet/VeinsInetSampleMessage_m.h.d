d1137552bd1b0d93790f6fc59513cee198daa4c0
Nahngom Zyegan Naam <nahngompansha@gmail.com>
2025-06-06 14:15:12 +0530

    Delete v2xsimulation directory

--- v2xsimulation/out/gcc-release/src/veins_inet/VeinsInetSampleMessage_m.h.d
+++
@@ -1,9 +0,0 @@
-veins_inet/VeinsInetSampleMessage_m.cc veins_inet/VeinsInetSampleMessage_m.h : \
-	veins_inet/VeinsInetSampleMessage.msg \
-	C:/Dev/inet-4.2.1/src\inet/common/INETDefs.msg \
-	C:/Dev/inet-4.2.1/src\inet/common/Units.msg \
-	C:/Dev/inet-4.2.1/src\inet/common/packet/chunk/Chunk.msg
-veins_inet/VeinsInetSampleMessage.msg:
-C:/Dev/inet-4.2.1/src\inet/common/INETDefs.msg:
-C:/Dev/inet-4.2.1/src\inet/common/Units.msg:
-C:/Dev/inet-4.2.1/src\inet/common/packet/chunk/Chunk.msg:
